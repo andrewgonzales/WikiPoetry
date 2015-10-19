5e5af216cd4007e5e300aab978a01bcd80e95d82
Ryan Chen <Ryan@Ryans-MacBook-Pro-3.local>
2015-10-19 12:16:51 -0700

    feat/routing added new line at end of files

--- client/app/app.js
+++ client/app/app.js
@@ -3,18 +3,18 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route
-
-var history = require('history');
-var useBasename = history.useBasename;
-var createHistory = history.createHistory;
+//for history within browser
+// var history = require('history');
+// var useBasename = history.useBasename;
+// var createHistory = history.createHistory;
 //require route file
 var routes = require('./routes');
 
 
-//Perform routing
-var base = useBasename(createHistory)({
-  basename: '/#'
-});
+// //Perform routing
+// var base = useBasename(createHistory)({
+//   basename: '/#'
+// });
 
 
 // Render FluxCartApp Controller View
