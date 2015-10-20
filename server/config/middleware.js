47970d4ae3d42591cf43869f3d7a586bb32c5ea2
Tomio Mizoroki <tmizoroki@gmail.com>
2015-10-19 22:36:34 -0700

    Build server skeleton

---
+++ server/config/middleware.js
@@ -0,0 +1,17 @@
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+module.exports = function (app, express) {
+  
+  var rnnRouter = express.Router();
+
+  app.use(morgan('dev'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  console.log(__dirname + '/../../client');
+  app.use(express.static(__dirname + '/../../dist'));
+
+  app.use('/api/rnn', rnnRouter);
+
+  require('../rnn/rnnRoutes.js')(rnnRouter);
+};
