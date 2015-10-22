f8600e37f2da54f2875f59ae420110e719df4d2e
rckchen <rchen1991@gmail.com>
2015-10-21 20:01:18 -0700

    feat/homeAJAX ajax request complete

--- client/app/components/home/HomeContent.react.js
+++ client/app/components/home/HomeContent.react.js
@@ -3,25 +3,53 @@ var Featured = require('./Featured.react');
 var InTheNews = require('./InTheNews.react');
 var DidYouKnow = require('./DidYouKnow.react');
 var OnThisDay = require('./OnThisDay.react');
+var $ = require('jquery');
+
 
 var HomeContent = React.createClass({
+  //set initial state
+  getInitialState: function () {
+    return {};
+  },
+  //Let components render first then perform AJAX request
+  componentDidMount: function () {
+    $.ajax({
+      url: '/api/rnn/wiki',
+      type: 'GET',
+      success: function(data) {
+        console.log('Wiki request sent');
+        this.setState({
+          Featured: data,
+          DidYouKnow: data,
+          InTheNews: data,
+          OnThisDay: data
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.log(status) ;
+        console.log(err.toString());
+        console.log(xhr)
+      }
+    });
+  },
+
   render: function () {
     return (
       <div>
         <section className="leftarticlegroup six columns">
           <article className="homearticle featured">
-            <Featured />
+            <Featured wikiData={this.state.Featured}/>
           </article>
           <article className="homearticle inthenews">
-            <DidYouKnow />
+            <DidYouKnow wikiData={this.state.DidYouKnow}/>
           </article>
         </section>
         <section className="rightarticlegroup row six columns">
           <article className="homearticle didyouknow">
-            <InTheNews />
+            <InTheNews wikiData={this.state.InTheNews}/>
           </article>
           <article className="homearticle onthisday">
-            <OnThisDay />
+            <OnThisDay wikiData={this.state.OnThisDay}/>
           </article>
         </section>
       </div>
--- server/rnn/ashleyRnn.js
+++ server/rnn/ashleyRnn.js
@@ -9,6 +9,12 @@ module.exports = {
     var testString = ashley.getPoem('shakespeare', searchTerm);
     res.status(200).send(testString).end();
 
+  },
+
+  getWiki: function (req, res, next) {
+    console.log('wiki!');
+    var testString = ashley.getPoem('shakespeare', 'home');
+    res.status(200).send(testString).end();
   }
 
 }
