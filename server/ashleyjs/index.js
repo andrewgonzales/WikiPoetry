851652e1ca907d4a3d3db081548291990953c4e9
tscheys <tim.scheys@gmail.com>
2015-10-23 14:04:44 -0700

    fix uncommented code

--- server/ashleyjs/index.js
+++ server/ashleyjs/index.js
@@ -357,19 +357,19 @@ var loadType = function (type) {
 var getPoem = function (type, searchTerm) {
   // make ajax request
   var text = '', plain = '', entities = [], data = {};
-  // wiki.page.data(searchTerm, { content: true }, function(response) {
-  //   // convert html to text for nlp processing
-  //   // console.log(response.text);
-  //   text = htmlToText.fromString(response.text['*']);
-  //   // build object to send
-  //   data.headers = getHeaders(response.text);
-  //   // get entities (places, persons,..) from wikipedia page
-  //   entities = getEntities(text, 5);
-  //   // load model of requested type
-  //   loadType(type);
-  //   // ask Ashley for a sentence
-  //   return predictSentence(model, true, 2.5, searchTerm);
-  // });
+  wiki.page.data(searchTerm, { content: true }, function(response) {
+    // convert html to text for nlp processing
+    // console.log(response.text);
+    text = htmlToText.fromString(response.text['*']);
+    // build object to send
+    data.headers = getHeaders(response.text);
+    // get entities (places, persons,..) from wikipedia page
+    entities = getEntities(text, 5);
+    // load model of requested type
+    loadType(type);
+    // ask Ashley for a sentence
+    return predictSentence(model, true, 2.5, searchTerm);
+  });
 };
 getPoem('shakespeare', 'barack obama');
 exports.getPoem = getPoem;
