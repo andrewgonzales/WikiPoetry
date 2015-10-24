fc677032866e3b8d7fdd016e7290835a73eea731
tscheys <tim.scheys@gmail.com>
2015-10-23 19:27:43 -0700

    implement homepage wiki scraping

--- server/ashleyjs/index.js
+++ server/ashleyjs/index.js
@@ -7,6 +7,7 @@ var moment = require('moment');
 var request = require('request');
 var wiki = require('node-wikipedia');
 var htmlToText = require("html-to-text");
+var cheerio = require('cheerio');
 
 // Model parameters 
 var sample_softmax_temperature = Math.pow(10, 0.5); // how peaky model predictions should be
@@ -340,9 +341,43 @@ var getArticle = function(type, searchTerm) {
   // 
 }
 
+var getHomePageSection = function(divTag) {
+  var section = 'table#mp-upper div';
+  return $(section + divTag).first().text().split('\n');
+};
+
 var getHomePage = function() {
-  // get info from the real homepage 
+  var homepage = {
+    featured: {
+      link: '',
+      poem: ''
+    },
+    news: {
+      text: []
+    },
+    day: {
+      text: []
+    },
+    know: {
+      text: []
+    }
+  };
+  // get info from the real homepage
+  wiki.page.data('Main Page', {content: true}, function (response) {
+    $ = cheerio.load(response.text['*']);
+    // get link of featured article 
+    homepage.featured = $('#mp-tfa b a').attr('href');
+    // get text of in the news items 
+    homepage.news.text = getHomePageSection('#mp-itn ul')
+    // get text of on this day 
+    homepage.day.text = getHomePageSection('#mp-otd ul');
+    // get search terms for did you know
+    homepage.know.text = getHomePageSection('#mp-dyk ul');
+    // return homepage object
+    return homepage;
+  });
 }
+getHomePage();
 
 var loadType = function (type) {
   // get correct model from output folder
@@ -368,7 +403,7 @@ var getPoem = function (type, searchTerm) {
     return predictSentence(model, true, 2.5, searchTerm);
   });
 };
-getPoem('shakespeare', 'barack obama');
+getPoem('shakespeare', 'love');
 exports.getPoem = getPoem;
 
 
