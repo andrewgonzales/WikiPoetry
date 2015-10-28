4c0e0d636d5638d9b123e7b28c0803c65b531aad
tscheys <tim.scheys@gmail.com>
2015-10-28 16:51:03 -0700

    fix server

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -9,7 +9,7 @@ module.exports = function (app, express) {
   app.use(bodyParser.json());
   console.log(__dirname + '/../../client');
   console.log(__dirname + '/../../dist');
-  app.use(express.static(__dirname + '/../../dist'));
+  // app.use(express.static(__dirname + '/../../dist'));
 
   app.use('/api/rnn', rnnRouter);
 
--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 require('./config/middleware.js')(app, express);
-require('./database.js');
+// require('./database.js');
 
-if(!module.parent){ 
- app.listen(port);
-}
 
 // app.use('/test', function(req, res) {
 //   res.send('This is it');
 // });
 
 app.use('/', express.static(__dirname + '/../../dist'));
+if(!module.parent){ 
+ app.listen(port);
+}
 
 module.exports = app;
