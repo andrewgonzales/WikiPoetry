185c7af25c9121fbd35e7a70d6ed88f757ac0848
tscheys <tim.scheys@gmail.com>
2015-10-28 17:04:01 -0700

    fix server

--- server/server.js
+++ server/server.js
@@ -5,16 +5,8 @@ var port = process.env.PORT || 8080;
 require('./config/middleware.js')(app, express);
 // require('./database.js');
 
-
-// app.use('/test', function(req, res) {
-//   res.send('This is it');
-// });
-
-// app.use(express.static(__dirname + '/../../dist'));
-
-app.use('/', function(req, res) {
-  res.send('this is it');
-});
+process.env.PWD = process.cwd();
+app.use(express.static(process.env.PWD + '/../../dist'));
 
 if(!module.parent){ 
  app.listen(port);
