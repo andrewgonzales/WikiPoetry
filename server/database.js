9467303e84a03bce2c9d43febdd21643c1af296d
tscheys <tim.scheys@gmail.com>
2015-10-29 14:42:28 -0700

    add schema for user in mongoDB

--- server/database.js
+++ server/database.js
@@ -1,5 +1,13 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/wikipoetry', function() {
-  console.log('mongoose connected');
+mongoose.connect('mongodb://localhost/wikipoetry');
+// create user schema
+var userSchema = new mongoose.Schema({
+  username: 'string',
+  hash: 'string',
+  email: 'string',
+  firstName: 'string',
+  lastName: 'string'
 });
+
+var User = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -2,9 +2,26 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 require('./config/middleware.js')(app, express);
+require('./database.js');
+ 
 // serve up statis files
 app.use(express.static(__dirname + '/dist'));
+passport.use(new GoogleStrategy({
+    consumerKey: GOOGLE_CONSUMER_KEY,
+    consumerSecret: GOOGLE_CONSUMER_SECRET,
+    callbackURL: "localhost:8080/auth/google/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({ username: profile.id }, function (err, user) {
+      console.log(err, user);
+      return done(err, user);
+    });
+    // create user of find him 
+    console.log('create user');
+  }
+));
 
 if (!module.parent) {
   app.listen(port);
