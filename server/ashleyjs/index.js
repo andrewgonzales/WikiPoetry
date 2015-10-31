ccd56a775fa05959baacd83a3b82eb2547f33cf8
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-10-31 16:03:24 -0700

    (feat) Removes console.log from getArticle function on API

--- server/ashleyjs/index.js
+++ server/ashleyjs/index.js
@@ -228,7 +228,7 @@ var getArticle = function(searchTerm, cb) {
   var article = {
     headings: [],
     picture: '',
-    pictureCaption: predictSentence(model, true, sample_softmax_temperature, searchTerm)
+    pictureCaption: predictSentence(model, true, sample_softmax_temperature, searchTerm);
   };
   wiki.page.data(searchTerm, {content: true}, function(response) {
     $ = cheerio.load(response.text['*']);
