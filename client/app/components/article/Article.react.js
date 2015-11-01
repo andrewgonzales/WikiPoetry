e463c64db57efb33341c6b2b42ef486aeb8a7b74
Tomio Mizoroki <tmizoroki@gmail.com>
2015-10-31 17:25:35 -0700

    Fix merge conflicts

--- client/app/components/article/Article.react.js
+++ client/app/components/article/Article.react.js
@@ -2,6 +2,7 @@
 //Should we serve a random poem when user clicks this link?
 
 var React = require('react');
+var ReactRouter = require('react-router');
 var ArticleSubsection = require('./ArticleSubsection.react');
 var ArticleImage = require('./ArticleImage.react');
 var ArticleIntro = require('./ArticleIntro.react');
@@ -14,6 +15,8 @@ function getSearchTerm () {
 
 var Article = React.createClass({
 
+  mixins: [ReactRouter.History],
+
   getInitialState: function () {
     return {
       term: getSearchTerm(),
@@ -21,25 +24,67 @@ var Article = React.createClass({
     }
   },
 
+<<<<<<< Updated upstream
   componentDidMount: function () {
     WikiPoetryStore.addChangeListener(this._onChange);
   },
 
   componentWillUnmount: function () {
     WikiPoetryStore.removeChangeListener(this._onChange);
+=======
+  handleClick: function (word) {
+    console.log('handle click');
+    console.log('outside', word);
+    API.getArticlePage(this.state.type, word, function (data) {
+      console.log('inside', word);
+      data.term = word;
+      this.history.pushState(data, '/Article/' + word, null );
+    }.bind(this));
+  },
+
+  linkifyArticle: function (content, links) {
+    console.log('linkify artilce');
+    var words = content.split(' ');
+    var index;
+    var spaced = [];
+    var linkedArray = words.map(function(word, i) {
+      if (links.indexOf(word) !== -1) {
+        index = links.indexOf(word);
+        var linkedWord = React.createElement("a", {key: i, onClick: function(){this.handleClick(word)}.bind(this), activeClassName: "link-active"}, word);
+        return linkedWord;
+      } else {
+        return word;
+      }
+    }.bind(this));
+    linkedArray.forEach(function(strOrObj) {
+      spaced.push(strOrObj);
+      spaced.push(' ');
+    });
+    return spaced;
+>>>>>>> Stashed changes
   },
 
   render: function () {
     var newInfo = this.props.location.state;
+<<<<<<< Updated upstream
     var searchTerm = this.props.routeParams.term;
     var articleType = this.state.type;
+=======
+    var searchTerm = this.state.term;
+
+    var linkedPoem = this.linkifyArticle(newInfo.poem, newInfo.replaced)
+>>>>>>> Stashed changes
     
     return (
       <div className="ten columns" id="article">
         <div className="article-container">
           <h3 className="article-title">{newInfo.term}</h3>
           <ArticleImage picture={newInfo.picture} />
+<<<<<<< Updated upstream
           <ArticleIntro term={searchTerm} type={articleType}/>
+=======
+          <p>{linkedPoem}</p>
+>>>>>>> Stashed changes
           {newInfo.headings.map(function (heading, i) {
             return (
               <ArticleSubsection
--- client/app/components/article/ArticleSubsection.react.js
+++ client/app/components/article/ArticleSubsection.react.js
@@ -1,8 +1,11 @@
 var React = require('react');
 var API = require('../../api/wikiApi');
 var WikiPoetryStore = require('../../stores/WikiPoetryStore');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 function getSearchTerm () {
+  console.log('get search term');
   return { 
     term: WikiPoetryStore.getTerm()
   }
@@ -10,7 +13,10 @@ function getSearchTerm () {
 
 var ArticleSubsection = React.createClass({
 
+  mixins: [ReactRouter.History],
+
   getInitialState: function () {
+    console.log('get init state');
     return {
       type: WikiPoetryStore.getType(),
       subContent: '',
@@ -19,6 +25,7 @@ var ArticleSubsection = React.createClass({
   },
 
   componentDidMount: function () {
+    console.log('comp did mount');
     API.getArticle({type: this.state.type, term: this.props.term}, function (data) {
       this.setState({
         subContent: data.poem,
@@ -29,6 +36,8 @@ var ArticleSubsection = React.createClass({
 
   componentWillReceiveProps: function (nextProps) {
     //To erase page before AJAX request enters new poem
+    
+    console.log('will reeive props');
     this.setState({
       subContent: ''
     });
@@ -41,12 +50,51 @@ var ArticleSubsection = React.createClass({
     }.bind(this));
   },
 
+  handleClick: function (word) {
+    console.log('handle click');
+    console.log('outside', word);
+    API.getArticlePage(this.state.type, word, function (data) {
+      console.log('inside', word);
+      data.term = word;
+      this.history.pushState(data, '/Article/' + word, null );
+    }.bind(this));
+  },
+
+  linkifyArticle: function (content, links) {
+    console.log('linkify artilce');
+    var words = content.split(' ');
+    var index;
+    var spaced = [];
+    var linkedArray = words.map(function(word, i) {
+      if (links.indexOf(word) !== -1) {
+        index = links.indexOf(word);
+        var linkedWord = React.createElement("a", {key: i, onClick: function(){this.handleClick(word)}.bind(this), activeClassName: "link-active"}, word);
+        return linkedWord;
+      } else {
+        return word;
+      }
+    }.bind(this));
+    linkedArray.forEach(function(strOrObj) {
+      spaced.push(strOrObj);
+      spaced.push(' ');
+    });
+    return spaced;
+  },
+
   render: function () {
+    console.log('render');
+    var content = this.state.subContent;
+    var links = this.state.replaced;
+    var linkedArticle;
+    if (content) {
+      linkedArticle = this.linkifyArticle(content, links);
+    }
+
     return (
       <div className="subsection">
         <div className="input">{this.props.error}</div>
         <h4 className="subheading">{this.props.subheading}</h4>
-        <p className="subcontent">{this.state.subContent}</p>
+        <p className="subcontent">{linkedArticle}</p>
       </div>
     );
   },
