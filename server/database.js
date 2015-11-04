2b4a9ef51d9a06aec46868a5525b1b3294726228
tscheys <tim.scheys@gmail.com>
2015-11-03 18:55:57 -0800

    fix server

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/wikipoetry';
+mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || process.env.MONGOLAB_URI || 'mongodb://localhost/wikipoetry';
 mongoose.connect(mongoURI);
 
 var db = mongoose.connection;
@@ -8,9 +8,5 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
  console.log('Mongodb connection open');
 });
-
-// User.login = function(user, pass, cb) {
-//   mongoose 
-// };
 
 module.exports = db;
