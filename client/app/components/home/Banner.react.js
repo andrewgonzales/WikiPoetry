d9f687747d1582422fd1b466a7ee9939371257ba
tscheys <tim.scheys@gmail.com>
2015-11-06 16:02:17 -0800

    style navbar

--- client/app/components/home/Banner.react.js
+++ client/app/components/home/Banner.react.js
@@ -37,7 +37,7 @@ var Banner = React.createClass({
       <table id="categories">
         <tr><td><h3>Welcome to WikiPoetry{text}</h3></td><td></td><td></td></tr>
         <tr><td><a key={1} onClick={this.handleClick.bind(this.search, 'number')} href="#" activeClassName="link-active">5.000.000</a> articles of informative poetry</td><td><a>Arts</a></td><td><a>Geography</a></td></tr>
-        <tr><td>Your number one source for computer generated poetry.</td><td><a>Mathematics</a></td><td><a>History</a></td></tr>
+        <tr><td>Your number one source for computer generated <a>poetry</a></td><td><a>Mathematics</a></td><td><a>History</a></td></tr>
         <tr><td></td><td><a>Science</a></td><td><a>Biography</a></td></tr>
       </table>
     );
